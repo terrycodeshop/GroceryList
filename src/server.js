1fef939278d82f1d1a3cbde08003dac2b4673529
miketerry1030 <miketerry1030@gmail.com>
2022-09-11 10:39:40 -0400

    Restructured app and folders

---
+++ src/server.js
@@ -0,0 +1,32 @@
+/* */
+
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Grocery List",
+    name: "Terry Code Shop",
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
+});
