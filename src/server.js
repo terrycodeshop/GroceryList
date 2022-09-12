4b0413436e54366ddfa94aad3137dac5b0453d98
miketerry1030 <miketerry1030@gmail.com>
2022-09-11 22:57:01 -0400

    adding mongo db connection

--- src/server.js
+++ src/server.js
@@ -1,11 +1,28 @@
 /* */
 
+/*( environment variables */
+const port = process.env.PORT || 3000;
+const mongo_uri =
+  "mongodb+srv://doadmin:UB70V68mO1253Kyt@tcs-mongo-7d8a1ff1.mongo.ondigitalocean.com/admin?tls=true&authSource=admin";
+
+/* import all node modules */
 const path = require("path");
 const express = require("express");
+const bodyparser = require("body-parser");
 const hbs = require("hbs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
+const multer = require("multer");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const store = new MongoDBStore({
+  uri: mongo_uri,
+  collection: "sessions",
+});
+const csrfProtection = csrf();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -27,6 +44,16 @@ app.get("", (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log("Server is up on port " + port);
-});
+/*connect to mongo database and then begin listening for connections */
+mongoose
+  .connect(mongo_uri)
+  .then((result) => {
+    console.log("Connected to database");
+
+    app.listen(3000, () => {
+      console.log("Server is listening on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
